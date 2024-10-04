38fcc60fcd44a23e34af145d2d247091f9d63d5d
charlotte sucias shepherd <charlotte_sucias@hotmail.co.uk>
2024-10-04 09:27:33 +0100

    finished backend

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,2 +1,7 @@
 class Owner < ApplicationRecord
+  has_many :cars
+  
+
+  validates :nickname, presence: true
+  validates :nickname, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Car.destroy_all
+Owner.destroy_all
+
+owner1 = Owner.create(nickname: "Bondie")
+owner2 = Owner.create(nickname: "yeah_baby")
+
+Car.create(brand: "Astin Martin", model: "DB9", year: "2005", fuel: "petrol", owner: owner1)
+Car.create(brand: "Mini Roadster", model: "R59", year: "2013", fuel: "petrol", owner: owner2)
